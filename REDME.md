6981b81dd170bae85b11bc99c9f80d3d46feae7a
LucasVazChamorro <lucasvazchamorro7@gmail.com>
2024-11-23 20:56:49 -0300

    Add contas

---
+++ REDME.md
@@ -0,0 +1,12 @@
+Este projeto se refere ao desenvolvimento de um Banco Digital em que deve possuir os seguintes tópicos:
+
+* Conta Corrente;
+* Conta Poupança;
+
+As contas devem permitir que o usuário:
+* Depósito;
+* Saque;
+* Transferência (entre as contas da própria instituição).
+
+
+O programa deve ser realizado na linguagem Java e deve respeitar o paradigma Orientado a Objeto.
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    public static void main(String[] args) {
+        Cliente lucas = new Cliente();
+        lucas.setNome("Lucas");
+
+        Conta cc = new ContaCorrente(lucas);
+        Conta poupanca = new ContaPoupanca(lucas);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+
+
+        Cliente leticia = new Cliente();
+        leticia.setNome("Leticia");
+
+        Conta cc2 = new ContaCorrente(leticia);
+        Conta poupanca2 = new ContaPoupanca(leticia);
+
+        cc2.depositar(400);
+        cc2.transferir(350, poupanca2);
+
+        cc2.imprimirExtrato();
+        poupanca2.imprimirExtrato();
+
+    }
 }
